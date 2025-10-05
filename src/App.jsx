46cb370c8a6dcad5805b6bf8ce2469e7ee6cfef2
MIKOXO <mikermount19@yahoo.com>
2025-10-05 12:30:20 +0300

    Animations added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import SubscribeForm from "./components/SubscribeForm";
 import SuccessCard from "./components/SuccessCard";
+import { AnimatePresence, motion } from "framer-motion";
 
 const App = () => {
   const [subscribed, setSubscribed] = useState(false);
@@ -8,16 +9,34 @@ const App = () => {
 
   return (
     <main className="mx-auto container flex flex-col justify-center items-center min-h-screen">
-      {!subscribed ? (
-        <SubscribeForm
-          onSubscribe={(email) => {
-            setEmail(email);
-            setSubscribed(true);
-          }}
-        />
-      ) : (
-        <SuccessCard email={email} onDismiss={() => setSubscribed(false)} />
-      )}
+      <AnimatePresence mode="wait">
+        {!subscribed ? (
+          <motion.div
+            key="Subscribe"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -30 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+          >
+            <SubscribeForm
+              onSubscribe={(email) => {
+                setEmail(email);
+                setSubscribed(true);
+              }}
+            />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="Success"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -30 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+          >
+            <SuccessCard email={email} onDismiss={() => setSubscribed(false)} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 };
