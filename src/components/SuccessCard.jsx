a39fc2ad18e0f965d92e35b17b72cf008491dde9
MIKOXO <mikermount19@yahoo.com>
2025-10-05 12:15:55 +0300

    Minor changes made on success card

--- src/components/SuccessCard.jsx
+++ src/components/SuccessCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import iconList from "../assets/icon-list.svg";
 
-const SuccessCard = () => {
+const SuccessCard = ({ email, onDismiss }) => {
   return (
     <div className="px-10 py-4 flex flex-col gap-3 items-start bg-White rounded-3xl shadow-md max-w-[450px]">
       <img src={iconList} alt="Success icon" className="w-[60px] mt-8" />
@@ -10,13 +10,13 @@ const SuccessCard = () => {
       </h1>
       <p className="font-medium text-Blue800 lg:w-[90%]">
         A confirmation email has been sent to{" "}
-        <span className="font-bold">youremail@example.com</span>. Please click
-        the link in the email to confirm your subscription.
+        <span className="font-bold">{email}</span>. Please click the link in the
+        email to confirm your subscription.
       </p>
 
       {/* Animated button */}
       <button
-        type="submit"
+        onClick={onDismiss}
         className="mb-8 mt-4 relative w-full group font-bold bg-Blue800 text-White p-4 rounded-lg hover:bg-gradient-to-r from-[#ff5279] via-[#ff6155] to-[#ff7c4d] transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-[#ff5279]/50 "
       >
         {/* top blur (pinkish) */}
